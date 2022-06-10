48413242c43dd737311cc0c36334d0efa5ad3d4b
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-06-09 21:47:05 -0300

    Realizar tarefa tv-shows-search

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const handleSearch = async (event) => {
   event.preventDefault();
 
-  // implemente a consulta a partir daqui
-
-  //// Exemplo de endpoint: https://api.tvmaze.com/search/shows?q=lost
-
-  //// Elementos de leiaute importantes:
-
-  //  #message: use para exibir mensagens aos usuário, por exemplo:
-
+  const shows = document.querySelector('#shows');
   const message = document.querySelector('#message');
-  message.innerHTML = 'exercício ainda não resolvido.';
 
-  //  #shows: conterá os shows, cada um em um <li>, por exemplo:
-  // <li>
-  //   <img class="poster" src="https://static.tvmaze.com/uploads/images/medium_portrait/0/1389.jpg" />
-  //   <span class="show-name">Lost</span>
-  // </li>
+  shows.innerHTML = '';
+  message.innerHTML = 'searching...';
+
+  const query = document.querySelector('#query').value;
+  const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+  const response = await fetch(url);
+  const result = await response.json();
+
+  if (result.length === 0) {
+    message.innerHTML = 'not found';
+    return;
+  }
+
+  message.innerHTML = '';
+
+  result.forEach((entry) => {
+    shows.insertAdjacentHTML(
+      'beforeend',
+      `
+      <li>
+        <img class="poster" src="${entry?.show?.image?.medium || ''}" />
+        <span class="show-name">${entry?.show?.name || ''}</span>
+      </li>`
+    );
+  });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
